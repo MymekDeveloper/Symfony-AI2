84476de09cdef5ba12e3e574377b96cf6dc920d9
MymekDeveloper <szwed.szymon1@gmail.com>
2024-01-09 21:47:50 +0100

    Created new serive, WeatherUtil, and created 2 commands to get weather from id and name of location

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -4,16 +4,22 @@ namespace App\Controller;
 
 use App\Entity\Location;
 use App\Repository\MeasurementRepository;
+use App\Service\WeatherUtil;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WeatherController extends AbstractController
 {
-    #[Route('/weather/{city}', name: 'app_weather', requirements: ['id' => '\d+'])]
-    public function city(Location $location, MeasurementRepository $repository): Response
+    #[Route('/weather/{country}/{city}', name: 'app_weather', requirements: ['id' => '\d+'])]
+    public function city(
+        #[MapEntity(mapping: ['country' => 'country', 'city' => 'city'])]
+        Location $location,
+        WeatherUtil $util,
+    ): Response
     {
-        $measurements = $repository->findByLocation($location);
+        $measurements = $util->GetWeatherForLocation($location);
 
         return $this->render('weather/city.html.twig', [
             'location' => $location,
